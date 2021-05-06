e9225afb1073bec7c79674299bb8ef84c9c41a70
bgrotegut21 <bgrotegut21>
2021-05-05 21:11:43 -0600

    rengineering matrix

--- objects.js
+++ objects.js
@@ -124,48 +124,43 @@ console.log(okIterator.next())
 console.log(okIterator.next())
 console.log(okIterator.next())
 
-class Matrix {
-    constructor(width, height, element = (x,y) => undefined){
+
+class Matrix{
+    constructor(width,height, element = (x,y) => undefined){
         this.width = width;
         this.height = height;
         this.content = [];
-        for (let y = 0; y < height; y++){
-            for (let x = 0; x < width; x++){
-                this.content[y *width + x] = element(x,y);
+        for(let x = 0; x < this.width; x++){
+            for(let y = 0; y < this.height; y++){
+                this.content[y * width + x] = element(x,y)
             }
         }
+        console.log(this.content);
     }
     get(x,y){
-        return this.content[y * this.width];
-    }
-
-}
-
-class MatrixIterator {
-    constructor(matrix){
-        this.x = 0;
-        this.y = 0;
-        this.matrix = matrix;
-    }
-    next(){
-        if(this.y == this.matrix.height) return {done: true};
-        let value = {x: this.x, y: this.y, value: this.matrix.get(this.x, this.y)}
-        this.x ++;
-
-        if (this.x == this.matrix.width){
-            this.x = 0;
-            this.y ++;
-        } 
-        return {value, done:false};
+        return this.content[y* width +x];
     }
 }
-Matrix.prototype[Symbol.iterator] = function(){
-    return  new MatrixIterator(this);
-}
+
+let matrix = new Matrix(2,2, (x,y) => `value ${x}, ${y}`);
 
 
 
-let matrix = new Matrix(3,5,(x,y) => `value ${x}, ${y}`);
-for (let {x,y,value} of matrix){
-    console.log(x,y,value);
+
+
+
+
+class Simple {
+    constructor(num1,num2){
+        this.num1 = num1;
+        this.num2 = num2;
+        let table = [];
+        for(let i = 0; i < num1; i++){
+            for(let e; e < num2; e++){
+                table.push(num1)
+                table.push()
+            }
+        }
+
+    }
 }
