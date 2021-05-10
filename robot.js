a28865a856ca27b8cc39b59836bdc3029f7aad0a
bgrotegut21 <bgrotegut21>
2021-05-09 21:00:15 -0600

    robot

--- robot.js
+++ robot.js
@@ -1,9 +1,18 @@
-const roads = ["Alice's House-Bob's House", "Alice's House Cabin","Alice's Home-Post-Office",
+const roads = ["Alice's House-Bob's House", "Alice's House-Cabin","Alice's Home-Post Office",
                 "Bob's House-Town Hall", "Daria's House-Ernie's House",
                 "Daria's House-Town Hall", "Ernie's House-Grete's House",
-                "Grete's House-Farm","Grete's House-Shop", "Marketplace-Farm", "Marketplace-Post-Office",
-                "Marketplace-Shop"
-
-
-
+                "Grete's House-Farm","Grete's House-Shop", "Marketplace-Farm", "Marketplace-Post Office",
+                "Marketplace-Shop", "Marketplace-Town Hall","Shop-Town-Hall"
 ]
+
+function buildGraph(graph){
+    let emptyObject = {};
+    let objectName = "";
+    for(let item of graph){
+        Items = splitedItem = item.split("-");
+        emptyObject[Items[0]] = Items[1];
+    }
+    return emptyObject;
+}
+console.log(buildGraph(roads))
+
