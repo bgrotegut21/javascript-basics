644984730c20343ea86fe4e6b183625f4d6f7f15
bgrotegut21 <bgrotegut21>
2021-05-29 14:25:48 -0600

    still finishing up expressions

--- expressions.js
+++ expressions.js
@@ -107,3 +107,23 @@ let matched;
 while (matched = number.exec(input)){
     console.log(`Found ${matched[0]} at ${matched.index}`)
 }
+
+function parseINI(string){
+    let result = {};
+    let section = result;
+    string.split(/\r?\n/).forEach(line => {
+        let match;
+        if (match = line.match(/^(\w+)=(.*)$/)){
+            section[match[1]] = match[2];
+        } else if (match = line.match(/^\[(.*)\]$/)) {
+            section = result[match[1]] = {};
+        } else if (!/^\s*(;.*)?$/.test(line)) {
+            throw new Error("Line'" + line+ "'is not valid");
+        }
+    });
+    return result;
+}
+
+console.log(parseINI(`name = Varsils 
+[adress] 
+city =Tessaloniki `))
